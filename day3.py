d8fc3b083e71c52a0064d7629322777b82596643
burcuckr <s230446408@stu.thk.edu.tr>
2026-01-23 16:43:17 +0300

    Create day3.py

---
+++ day3.py
@@ -0,0 +1,43 @@
+print(''' 
+  ___________________________________________________________________
+ |~ ~~ ~~~ ~ ~ ~~~ ~ _____.----------._ ~~~  ~~~~ ~~   ~~  ~~~~~ ~~~~|
+ |  _   ~~ ~~ __,---'_       "         `. ~~~ _,--.  ~~~~ __,---.  ~~|
+ | | \___ ~~ /      ( )   "          "   `-.,' (') \~~ ~ (  / _\ \~~ |
+ |  \    \__/_   __(( _)_      (    "   "     (_\_) \___~ `-.___,'  ~|
+ |~~ \     (  )_(__)_|( ))  "   ))          "   |    "  \ ~~ ~~~ _ ~~|
+ |  ~ \__ (( _( (  ))  ) _)    ((     \\//    " |   "    \_____,' | ~|
+ |~~ ~   \  ( ))(_)(_)_)|  "    ))    //\\ " __,---._  "  "   "  /~~~|
+ |    ~~~ |(_ _)| | |   |   "  (   "      ,-'~~~ ~~~ `-.   ___  /~ ~ |
+ | ~~     |  |  |   |   _,--- ,--. _  "  (~~  ~~~~  ~~~ ) /___\ \~~ ~|
+ |  ~ ~~ /   |      _,----._,'`--'\.`-._  `._~~_~__~_,-'  |H__|  \ ~~|
+ |~~    / "     _,-' / `\ ,' / _'  \`.---.._          __        " \~ |
+ | ~~~ / /   .-' , / ' _,'_  -  _ '- _`._ `.`-._    _/- `--.   " " \~|
+ |  ~ / / _-- `---,~.-' __   --  _,---.  `-._   _,-'- / ` \ \_   " |~|
+ | ~ | | -- _    /~/  `-_- _  _,' '  \ \_`-._,-'  / --   \  - \_   / |
+ |~~ | \ -      /~~| "     ,-'_ /-  `_ ._`._`-...._____...._,--'  /~~|
+ | ~~\  \_ /   /~~/    ___  `---  ---  - - ' ,--.     ___        |~ ~|
+ |~   \      ,'~~|  " (o o)   "         " " |~~~ \_,-' ~ `.     ,'~~ |
+ | ~~ ~|__,-'~~~~~\    \"/      "  "   "    /~ ~~   O ~ ~~`-.__/~ ~~~|
+ |~~~ ~~~  ~~~~~~~~`.______________________/ ~~~    |   ~~~ ~~ ~ ~~~~|
+ |____~____~__~_______~~_~____~~_____~~___~_~~___~\_|_/ ~_____~___~__|
+ / \----- ----- ------------  ------- ----- -------  --------  ------|
+ \_/_________________________________________________________________|''')
+print("Welcome to Treasure Island.\nYour mission is to find the treasure.")
+opt1 = input("You're at a cross road. Where do you want to go?\n left or right: ")
+if opt1 == "left":
+    print("You've come to a lake. There is an island in the middle of the lake.")
+    opt2 = input("To cross the lake, build a raft using tree branches or jump across the rocks. Type raft to build a raft, or rock to jump across the rocks.")
+    if opt2 == "rock":
+        print("Even though it was difficult, you managed to cross to the other side. Now there are three doors in front of you.")
+        opt3 = input(" Which one will you choose: red, yellow, or purple?")
+        if opt3 == "purple":
+            print("You found the treasure!")
+        elif opt3 == "red":
+            print("Behind the door… there was something. And it noticed you. Game Over.")
+        else:
+            print("Behind the door… there was nothing. You suddenly woke up from your sleep. There was no treasure. Game Over.")
+    else:
+        print("You chose to cut down the trees. Nature does not forgive those who harm it. Game Over!")
+else:
+    print("You fell into a hole. Game over!")
+
